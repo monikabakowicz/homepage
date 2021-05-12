59eb5985a6f4757669f6459dc4cb02aa1c617cb0
Monika Bakowicz <monikabakowicz1@gmail.com>
2021-05-13 01:33:10 +0200

    let -> const; converted onChangeImageClick

--- js/script.js
+++ js/script.js
@@ -27,14 +27,23 @@
 
 
     onHideTableClick = () => {
-        let table = document.querySelector(".table");
-        let changeTableButtonText = document.querySelector(".js-changeTableButtonText");
+        const table = document.querySelector(".table");
+        const changeTableButtonText = document.querySelector(".js-changeTableButtonText");
 
         table.classList.toggle("table--hidden");
         changeTableButtonText.innerText = table.classList.contains("table--hidden") ? "Show" : "Hide";
     };
 
 
+    onChangeImageClick = () => {
+        const section__image1 = document.querySelector(".js-section__image1");
+        const section__image2 = document.querySelector(".js-section__image2");
+
+        section__image1.classList.toggle("section__image--hidden");
+        section__image2.classList.toggle("section__image--hidden");
+    };
+
+   
 
     const init = () => {
         const changeThemeButton = document.querySelector(".js-changeThemeButton");
@@ -43,8 +52,11 @@
         const changeNameColorButton = document.querySelector(".js-changeNameColorButton");
         changeNameColorButton.addEventListener("click", toggleNameColor);
 
-        let hideTableButton = document.querySelector(".js-hideTableButton");
+        const hideTableButton = document.querySelector(".js-hideTableButton");
         hideTableButton.addEventListener("click", onHideTableClick);
+
+        const changeImageButton = document.querySelector(".js-changeImageButton");  
+        changeImageButton.addEventListener("click", onChangeImageClick);
 
         welcome();
     };
@@ -52,15 +64,3 @@
     init();
 
 }
-
-
-
-
-let changeImageButton = document.querySelector(".js-changeImageButton");
-let section__image1 = document.querySelector(".js-section__image1");
-let section__image2 = document.querySelector(".js-section__image2");
-
-changeImageButton.addEventListener("click", () => {
-    section__image1.classList.toggle("section__image--hidden");
-    section__image2.classList.toggle("section__image--hidden");
-});
